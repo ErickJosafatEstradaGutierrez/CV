01592fb3f8a9f48162aa1fc7c5cd9dc7b8c49ec6
Role GamerZ <Role523124@gmail.com>
2025-06-09 21:13:36 -0600

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>HOLA BUENAS NOCHES</h1>
+      <img 
+  src="https://assets.about.me/background/users/r/o/g/rogeliobautistasanchez_1465761673_77.jpg" 
+  alt="Imagen ejemplo" 
+  style={{ maxWidth: '300px', marginTop: '1rem' }} 
+/>
+
     </div>
   );
 }
